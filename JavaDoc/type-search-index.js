f7a1fcb81d7cbf709d9f3940ff394cdcb1588e26
João Duarte <71460038+jduarte98@users.noreply.github.com>
2023-07-30 18:14:25 +0100

    JavaDoc
    Generated!

---
+++ JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"ComparatorUtils"},{"p":"<Unnamed>","l":"ConsoleWriter"},{"p":"<Unnamed>","l":"FileComparison"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"PDFGenerator"},{"p":"<Unnamed>","l":"RetrievedFile"},{"p":"<Unnamed>","l":"UserData"}];updateSearchResults();
